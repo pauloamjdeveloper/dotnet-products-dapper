a0f2389a7f6081738f30a879c5fd3e2386b53ee5
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-09-09 15:42:42 -0300

    Criando lançamento de exceção para produto

---
+++ src/Products.Dapper.WebAPI/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Products.Dapper.WebAPI.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException() : base("Product not found.") {}
+
+        public ProductNotFoundException(string message) : base(message) {}
+    }
+}
