cac1f8006eb78eb0ec4e60c9996b5732bc435497
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-09-09 13:56:28 -0300

    Criando as classes de modelo exceção para produtos

---
+++ src/Products.Dapper.WebAPI/Exceptions/ModelExceptionValidation.cs
@@ -0,0 +1,15 @@
+﻿namespace Products.Dapper.WebAPI.Exceptions
+{
+    public class ModelExceptionValidation : Exception
+    {
+        public ModelExceptionValidation(string error) : base(error) { }
+
+        public static void When(bool hasErros, string error)
+        {
+            if (hasErros)
+            {
+                throw new ModelExceptionValidation(error);
+            }
+        }
+    }
+}
---
+++ src/Products.Dapper.WebAPI/Models/Product.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Products.Dapper.WebAPI.Exceptions;
+
+namespace Products.Dapper.WebAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+
+
+        public Product() {}
+
+        public Product(int id, string name, string description, decimal price, int quantity)
+        {
+            Validate(id, name, description, price, quantity);
+        }
+
+        private void Validate(int id, string name, string description, decimal price, int quantity)
+        {
+            ModelExceptionValidation.When(id <= 0, "Invalid Id value");
+
+            ModelExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name, Name is required");
+
+            ModelExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
+
+            ModelExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid description. Description is required");
+
+            ModelExceptionValidation.When(description.Length < 5, "Invalid description, too short, minimum 5 characters");
+
+            ModelExceptionValidation.When(price <= 0, "Invalid price value");
+
+            ModelExceptionValidation.When(quantity <= 0, "Invalid stock value");
+
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
