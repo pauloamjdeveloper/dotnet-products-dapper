a8ebdd564afde1c45df4c029c51b5788a6b46540
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-09-10 13:54:45 -0300

    Criando classe para validar preço

---
+++ src/Products.Dapper.WebAPI/Utilities/DecimalRangeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Products.Dapper.WebAPI.Utilities
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class DecimalRangeAttribute : ValidationAttribute
+    {
+        private readonly decimal _minimum;
+        private readonly decimal _maximum;
+
+        public DecimalRangeAttribute()
+        {
+            _minimum = 0.01m;
+            _maximum = decimal.MaxValue;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is decimal decimalValue)
+            {
+                return decimalValue >= _minimum && decimalValue <= _maximum;
+            }
+            return false;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return $"The field {name} must be between {_minimum} and {_maximum}.";
+        }
+    }
+}
