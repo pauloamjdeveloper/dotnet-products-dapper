776c57796b431e950936722f9cb3a48ffbe60e05
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-09-10 10:42:00 -0300

    Aplicando a regra de negócio do produto

---
+++ src/Products.Dapper.WebAPI/Business/IProductBusiness.cs
@@ -0,0 +1,19 @@
+﻿using Products.Dapper.WebAPI.Models;
+
+namespace Products.Dapper.WebAPI.Business
+{
+    public interface IProductBusiness
+    {
+        Task<List<Product>> GetAllProductsAsync(); 
+        
+        Task<Product> GetProductByIdAsync(int id);
+
+        Task<Product> CreateProductAsync(Product product);
+
+        Task<Product> UpdateProductAsync(Product product);
+        
+        Task DeleteProductAsync(int id);
+
+        Task<bool> ExistsAsync(int id);
+    }
+}
---
+++ src/Products.Dapper.WebAPI/Business/Implementations/ProductBusiness.cs
@@ -0,0 +1,46 @@
+﻿using Products.Dapper.WebAPI.Models;
+using Products.Dapper.WebAPI.Repositories;
+
+namespace Products.Dapper.WebAPI.Business.Implementations
+{
+    public class ProductBusiness : IProductBusiness
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductBusiness(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<List<Product>> GetAllProductsAsync()
+        {
+            return await _productRepository.GetAllAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _productRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Product> CreateProductAsync(Product product)
+        {
+            return await _productRepository.CreateAsync(product);
+        }
+
+        public async Task<Product> UpdateProductAsync(Product product)
+        {
+            return await _productRepository.UpdateAsync(product);
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            await _productRepository.DeleteAsync(id);
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            return product != null;
+        }
+    }
+}
